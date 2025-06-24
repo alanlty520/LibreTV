9878fbc40daf141989f0a41133a892c260f3e79e
alanlty520 <alanlty@126.com>
2025-06-24 20:25:01 +0800

    Update index-page.js

--- js/index-page.js
+++ js/index-page.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 try {
                     window.history.replaceState(
                         { search: keyword }, 
-                        `搜索: ${keyword} - LibreTV`, 
+                        `搜索: ${keyword} - 孤狼影视`, 
                         window.location.href
                     );
                 } catch (e) {
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
             try {
                 window.history.replaceState(
                     { search: searchQuery }, 
-                    `搜索: ${searchQuery} - LibreTV`, 
+                    `搜索: ${searchQuery} - 孤狼影视`, 
                     `/s=${encodeURIComponent(searchQuery)}`
                 );
             } catch (e) {
